f2649aa61be9be626078a2d64ddd8a208f4149fb
Karim Mammadli <kmammadl@purdue.edu>
2024-01-20 17:45:20 -0500

    SIGN UP ALL SCREEN DONE

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
-// import signUp from "../../../firebase/auth/signup";
+import signUp from "../../../firebase/auth/signup";
 import { useRouter } from "next/navigation";
-// import addData from "../../../firebase/addData";
+import addData from "../../../firebase/addData";
+import "flowbite";
 
 export default function SignUp() {
   const [email, setEmail] = React.useState("");
@@ -30,10 +31,14 @@ export default function SignUp() {
       weight: weight,
       height: height,
       age: age,
-      gender: gender
-    }
+      gender: gender,
+    };
 
-    const { resultInsert, errorInsert } = await addData('users', result?.user.uid, data)
+    const { resultInsert, errorInsert } = await addData(
+      "users",
+      result?.user.uid,
+      data
+    );
 
     if (errorInsert) {
       return console.log(errorInsert);
@@ -44,11 +49,12 @@ export default function SignUp() {
     return router.push("/");
   };
   return (
-    <section className="dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <section className="dark:bg-gray-900 min-h-screen">
+      <div className="flex flex-col items-center justify-center px-6 py-8 min-h-screen">
+        {/* lg:py-0 */}
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="flex justify-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign Up to BoilerPlate!
             </h1>
 
@@ -107,7 +113,6 @@ export default function SignUp() {
                 />
               </div>
 
-    
               <div>
                 <label
                   htmlFor="age"
@@ -126,7 +131,6 @@ export default function SignUp() {
                 />
               </div>
 
-
               <div>
                 <label
                   htmlFor="weight"
@@ -134,18 +138,21 @@ export default function SignUp() {
                 >
                   Weight
                 </label>
-                <input
-                  onChange={(e) => setWeight(e.target.value)}
-                  required
-                  type="weight"
-                  name="weight"
-                  id="weight"
-                  placeholder="Weight"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 
-                />
-            </div>
-
+                <div className="flex items-center">
+                  <input
+                    onChange={(e) => setWeight(e.target.value)}
+                    required
+                    type="weight"
+                    name="weight"
+                    id="weight"
+                    placeholder="Weight"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                  {/* add small letter saying lb beside weight */}
+                  <span className="ps-2 text-xs text-gray-400">lb</span>
+                </div>
+              </div>
 
               <div>
                 <label
@@ -154,37 +161,87 @@ export default function SignUp() {
                 >
                   Height
                 </label>
-                <input
-                  onChange={(e) => setHeight(e.target.value)}
-                  required
-                  type="height"
-                  name="height"
-                  id="height"
-                  placeholder="Height"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
-            </div>
 
-              <div>
+                <div className="flex items-center">
+                  <input
+                    onChange={(e) => setHeight(e.target.value)}
+                    required
+                    type="height"
+                    name="height"
+                    id="height"
+                    placeholder="Height"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                  <span className="ps-2 text-xs text-gray-400">ft</span>
+                </div>
+              </div>
+
+              <div className="pb-3">
                 <label
                   htmlFor="gender"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  gender
+                  Gender
                 </label>
                 <select
+                  id="gender"
                   value={gender}
                   onChange={(e) => setGender(e.target.value)}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 >
-                  <option value="select">Select</option>  
+                  <option defaultValue="Null">Choose your gender</option>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                 </select>
-              <button type="submit">Sign Up</button>
-            </div>
-            
+              </div>
+
+              <div className="flex justify-center">
+                {/* put it into the center */}
+                <button className="mx-auto">
+                  <a
+                    // href="#_"
+                    className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-black transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+                  >
+                    <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#CEB888] group-hover:h-full"></span>
+                    <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                      <svg
+                        className="w-5 h-5 text-black"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        // xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M14 5l7 7m0 0l-7 7m7-7H3"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                      <svg
+                        className="w-5 h-5 text-black"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        // xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M14 5l7 7m0 0l-7 7m7-7H3"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                      Sign up
+                    </span>
+                  </a>
+                </button>
+              </div>
             </form>
-            
           </div>
         </div>
       </div>
