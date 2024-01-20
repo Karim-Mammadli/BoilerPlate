fb23055314a8d94ee0dad72398ea4534553933e6
youngjun-yoo16 <yoo156@purdue.edu>
2024-01-20 03:24:56 -0500

    Lowercase change

--- src/app/firebase/auth/signup.tsx
+++ src/app/firebase/auth/signup.tsx
@@ -3,7 +3,7 @@ import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 
 const auth = getAuth(firebase_app);
 
-export default async function signIn(email: String, password: String) {
+export default async function signIn(email: string, password: string) {
     let result = null,
         error = null;
     try {
