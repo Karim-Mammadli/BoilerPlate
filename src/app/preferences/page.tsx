f0598141a3cddb821d668704fd336564f7973f6b
Aaryan Gautam <64756497+aaryan-gautam@users.noreply.github.com>
2024-01-20 05:30:35 -0500

    added preferences page

---
+++ src/app/preferences/page.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <div>
+      <p>Allergens</p>
+      <div className="space-y-4">
+        <button>
+          <input type="checkbox" id="Login22" className="hidden peer" />
+          <label htmlFor="Login22" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login22
+          </label>
+        </button>
+        
+        <button>
+          <input type="checkbox" id="Login23" className="hidden peer" />
+          <label htmlFor="Login23" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login23
+          </label>
+        </button>
+      </div>
+
+      <div className="space-y-4">
+        <button>
+          <input type="checkbox" id="Login24" className="hidden peer" />
+          <label htmlFor="Login24" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login24
+          </label>
+        </button>
+        
+        <button>
+          <input type="checkbox" id="Login25" className="hidden peer" />
+          <label htmlFor="Login25" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login25
+          </label>
+        </button>
+      </div>
+</div>
+<div>
+<p>Food Preferences</p>
+      <div className="space-y-4">
+        <button>
+          <input type="checkbox" id="Login26" className="hidden peer" />
+          <label htmlFor="Login26" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login26
+          </label>
+        </button>
+        
+        <button>
+          <input type="checkbox" id="Login27" className="hidden peer" />
+          <label htmlFor="Login27" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login27
+          </label>
+        </button>
+      </div>
+
+      <div className="space-y-4">
+        <button>
+          <input type="checkbox" id="Login28" className="hidden peer" />
+          <label htmlFor="Login28" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login28
+          </label>
+        </button>
+        
+        <button>
+          <input type="checkbox" id="Login29" className="hidden peer" />
+          <label htmlFor="Login29" className="cursor-pointer text-white bg-indigo-400 px-5 py-2.5 peer-checked:bg-red-500">
+            Login29
+          </label>
+        </button>
+      </div>
+</div>
+    </div>
+  );
+}
