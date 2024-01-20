05528990ebd4d34f55d0e143522afd9e85fb939e
Yashvardhan Khaitan <yashv.khaitan@gmail.com>
2024-01-20 03:31:52 -0500

    changed signup

--- src/app/firebase/auth/signup.tsx
+++ src/app/firebase/auth/signup.tsx
@@ -1,13 +1,14 @@
 import firebase_app from "../config";
-import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 const auth = getAuth(firebase_app);
 
-export default async function signIn(email: string, password: string) {
+
+export default async function signUp(email: string, password: string) {
     let result = null,
         error = null;
     try {
-        result = await signInWithEmailAndPassword(auth, email, password);
+        result = await createUserWithEmailAndPassword(auth, email, password);
     } catch (e) {
         error = e;
     }
