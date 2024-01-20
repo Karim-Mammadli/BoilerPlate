0fca05340eb56cfe11c392d09a1a71dc9b78b326
youngjun-yoo16 <yoo156@purdue.edu>
2024-01-20 13:21:36 -0500

    API done

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -33,7 +33,7 @@ export default function SignUp() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign Up to BoilerPlate !
+              Sign Up to BoilerPlate!
             </h1>
 
             <form className="space-y-4 md:space-y-6" onSubmit={handleForm}>
@@ -133,10 +133,10 @@ export default function SignUp() {
 
               <div>
                 <label
-                  htmlFor="weight"
+                  htmlFor="height"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Weight
+                  Height
                 </label>
                 <input
                   onChange={(e) => setHeight(e.target.value)}
